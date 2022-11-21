74118e3b2b36c13adb7630966d27e302be046fd2
Alazar Zewdu <alazarzewdu3@gmail.com>
2022-11-21 16:35:04 +0300

    added icons

--- app/src/main/java/com/example/twitter_clone/MainActivity.java
+++ app/src/main/java/com/example/twitter_clone/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.twitter_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
 
 public class MainActivity extends AppCompatActivity {
+
+Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,App.class);
+                startActivity(intent);
+                finish();
+            }
+
+        },3000);
+
+
     }
 }
